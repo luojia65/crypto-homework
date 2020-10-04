013bc2ee2a1f0e54b2f642eb4c144b409d8c7bf4
luojia65 <me@luojia.cc>
2020-10-04 22:06:13 +0800

    Update

--- 9.cpp
+++ 9.cpp
@@ -1,11 +1,44 @@
-//#include <gmpxx.h>
+#include <openssl/evp.h>
+#include <openssl/pkcs7.h>
 #include <iostream>
 #include <stdio.h>
 #define FOR(x,f,t) for(x=f;x<=t;++x)
 using namespace std;
-//#define ll mpz_t
+
+char CA[]="\
+-----BEGIN CERTIFICATE----- \n\
+MIIB/zCCAaagAwIBAgIJAKKa0PAt9M1FMAoGCCqBHM9VAYN1MFsxCzAJBgNVBAYT \n\
+AkNOMQ4wDAYDVQQIDAVIdUJlaTEOMAwGA1UEBwwFV3VIYW4xDTALBgNVBAoMBEhV \n\
+U1QxDDAKBgNVBAsMA0NTRTEPMA0GA1UEAwwGY2Fyb290MB4XDTIwMDkyMDIwNTkx \n\
+OVoXDTMwMDkxODIwNTkxOVowWzELMAkGA1UEBhMCQ04xDjAMBgNVBAgMBUh1QmVp \n\
+MQ4wDAYDVQQHDAVXdUhhbjENMAsGA1UECgwESFVTVDEMMAoGA1UECwwDQ1NFMQ8w \n\
+DQYDVQQDDAZjYXJvb3QwWTATBgcqhkjOPQIBBggqgRzPVQGCLQNCAASJ8mm28JJR \n\
+bZKLr6DCo1+KWimpKEsiTfZM19Zi5ao7Au6YLosyN71256MWmjwkwXxJeLa0lCfm \n\
+kF/YWCX6qGQ0o1MwUTAdBgNVHQ4EFgQUAL5hW3RUzqvsiTzIc1gUHeK5uzQwHwYD \n\
+VR0jBBgwFoAUAL5hW3RUzqvsiTzIc1gUHeK5uzQwDwYDVR0TAQH/BAUwAwEB/zAK \n\
+BggqgRzPVQGDdQNHADBEAiAaZMmvE5zzXHx/TBgdUhjtpRH3Jpd6OZ+SOAfMtKxD \n\
+LAIgdKq/v2Jkmn37Y9U8FHYDfFqk5I0qlQOAmuvbVUi3yvM= \n\
+";
+
+char PK[]="\
+-----BEGIN EC PARAMETERS----- \n\
+BggqgRzPVQGCLQ== \n\
+-----END EC PARAMETERS----- \n\
+-----BEGIN EC PRIVATE KEY----- \n\
+MHcCAQEEINQhCKslrI3tKt6cK4Kxkor/LBvM8PSv699Xea7kTXTToAoGCCqBHM9V \n\
+AYItoUQDQgAEH7rLLiFASe3SWSsGbxFUtfPY//pXqLvgM6ROyiYhLkPxEulwrTe8 \n\
+kv5R8/NA7kSSvcsGIQ9EPWhr6HnCULpklw== \n\
+-----END EC PRIVATE KEY----- \n\
+";
+
+char input[10005];
 
 int main() {
-	
+    char c;int l=0; while(c=getchar(),c!=EOF) input[l++]=c;
+	OpenSSL_add_all_algorithms();
+
+    PKCS7* p7 = PKCS7_new();
+
+
     return 0;
 }
